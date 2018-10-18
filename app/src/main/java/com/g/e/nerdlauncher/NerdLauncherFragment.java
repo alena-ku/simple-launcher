f49a4890682bdf6a5548de39d5d5ee53fb28c981
alena <49859442+alena-dev@users.noreply.github.com>
2018-10-18 19:38:26 +0300

    Task-1. Receive launchable apps list

--- app/src/main/java/com/g/e/nerdlauncher/NerdLauncherFragment.java
+++ app/src/main/java/com/g/e/nerdlauncher/NerdLauncherFragment.java
@@ -1,16 +1,25 @@
 package com.g.e.nerdlauncher;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.List;
+
 public class NerdLauncherFragment extends Fragment {
+
+    private static final String TAG = "NerdLauncherFragment";
+
     private RecyclerView mRecyclerView;
 
 
@@ -26,6 +35,21 @@ public class NerdLauncherFragment extends Fragment {
         mRecyclerView = view.findViewById(R.id.app_recycler_view);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
+        setupAdapter();
+
         return view;
     }
+
+    private void setupAdapter() {
+        Intent startupIntent = new Intent(Intent.ACTION_MAIN);
+        startupIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        PackageManager packageManager = getActivity().getPackageManager();
+        List<ResolveInfo> activities = packageManager
+                .queryIntentActivities(startupIntent, 0);
+
+        Log.i(TAG, "Found " + activities.size() + " activities.");
+    }
+
+
 }
