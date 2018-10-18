50ff80ee27d744643fb0b61ec1ba56490ce0d063
alena <49859442+alena-dev@users.noreply.github.com>
2018-10-18 19:04:41 +0300

    Task-1. Implement fragment and add RecyclerView

---
+++ app/src/main/java/com/g/e/nerdlauncher/NerdLauncherFragment.java
@@ -0,0 +1,31 @@
+package com.g.e.nerdlauncher;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class NerdLauncherFragment extends Fragment {
+    private RecyclerView mRecyclerView;
+
+
+    public static NerdLauncherFragment createInstance() {
+        return new NerdLauncherFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_nerd_launcher, container, false);
+
+        mRecyclerView = view.findViewById(R.id.app_recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/g/e/nerdlauncher/SingleFragmentActivity.java
@@ -0,0 +1,34 @@
+package com.g.e.nerdlauncher;
+
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    protected  abstract  android.support.v4.app.Fragment createFragment();
+
+    @LayoutRes
+    protected int getLayoutResId(){
+        return R.layout.activity_fragment;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResId());
+
+        android.support.v4.app.FragmentManager fragmentManager=
+                getSupportFragmentManager();
+        Fragment fragment=fragmentManager
+                .findFragmentById(R.id.fragment_container);
+
+        if(fragment==null){
+            fragment=createFragment();
+            fragmentManager.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
+}
